f0cc066110f25cf274cdcc2b3c5d2dadfb3b711f
kotaro chiba <chiba-kotaro@dmm.com>
2019-10-16 16:32:39 +0900

    update: react-redux hoooks

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,30 @@
 import * as React from 'react';
 import Layout from '~/components/Layout';
 
-import { bindActionCreators, Dispatch } from 'redux';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { counterActions } from '~/actions';
 import { RootState } from '~/reducers';
 
 import { CountState } from '~/stateTypes';
 
-interface IndexProps {
-    counter: CountState;
-    actions: typeof counterActions;
-}
+const Index: React.FC = () => {
+    const counter = useSelector<RootState, CountState>(state => state.counter);
+    const dispatch = useDispatch();
 
-const Index: React.FC<IndexProps> = ({ counter, actions }) => (
-    <Layout title="Top page">
-        <h1>Hello Next.js with Koa.js and Typescript</h1>
-        <div>count: {counter && counter.count}</div>
-        <div>
-            <button onClick={() => actions.increment(1)}>count up</button>
-            <button onClick={() => actions.decrement(1)}>count down</button>
-        </div>
-    </Layout>
-);
+    return (
+        <Layout title="Top page">
+            <h1>Hello Next.js with Koa.js and Typescript</h1>
+            <div>count: {counter && counter.count}</div>
+            <div>
+                <button onClick={() => dispatch(counterActions.increment(1))}>
+                    count up
+                </button>
+                <button onClick={() => dispatch(counterActions.decrement(1))}>
+                    count down
+                </button>
+            </div>
+        </Layout>
+    );
+};
 
-const mapStateToProps = (state: RootState) => ({
-    counter: state.counter
-});
-
-const mapDispatchToProps = (dispatch: Dispatch) => ({
-    actions: bindActionCreators(counterActions, dispatch)
-});
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(Index);
+export default Index;
