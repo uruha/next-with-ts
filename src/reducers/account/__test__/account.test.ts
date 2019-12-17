84cde0976ff8e74f4365eb0f23ee959d7e8095f7
kotaro chiba <chiba-kotaro@dmm.com>
2019-12-17 19:35:28 +0900

    add: account snapshot testing

---
+++ src/reducers/account/__test__/account.test.ts
@@ -0,0 +1,24 @@
+import snapshotDiff from 'snapshot-diff';
+import { accountActions } from '~/actions/account';
+
+import { initialAccount, account } from '~/reducers/account';
+import { Account } from '~/modelTypes';
+
+const testingAccount: Account = {
+    email: 'test@example.com',
+    nickname: 'snapshot testing'
+};
+
+describe('Account reducer', () => {
+    it('update account', () => {
+        expect(
+            snapshotDiff(
+                initialAccount,
+                account(
+                    initialAccount,
+                    accountActions.updateAccount(testingAccount)
+                )
+            )
+        ).toMatchSnapshot();
+    });
+});
