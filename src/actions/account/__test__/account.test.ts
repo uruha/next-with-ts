68c83094e091d7a3b122270e1921d70fdc1ce7ed
uruha <kochmusica@gmail.com>
2019-12-21 07:38:38 +0900

    add: account testing

---
+++ src/actions/account/__test__/account.test.ts
@@ -0,0 +1,33 @@
+import { GET_ACCOUNT, UPDATE_ACCOUNT } from '~/constant';
+import { Account } from '~/modelTypes';
+import { accountActions } from '~/actions/account';
+
+describe('Account actions', () => {
+    it('should create get account action', () => {
+        const expectedAction = {
+            type: GET_ACCOUNT
+        };
+
+        expect(accountActions.getAccount()).toEqual(expectedAction);
+    });
+
+    it('should create update accont action', () => {
+        const expectedPayload: Account = {
+            nickname: 'Test',
+            email: 'accout@test.com'
+        };
+        const expectedAction = {
+            type: UPDATE_ACCOUNT,
+            payload: {
+                data: {
+                    nickname: expectedPayload.nickname,
+                    email: expectedPayload.email
+                }
+            }
+        };
+
+        expect(accountActions.updateAccount(expectedPayload)).toEqual(
+            expectedAction
+        );
+    });
+});
