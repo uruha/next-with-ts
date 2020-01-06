aa5cfcc681cb0044697dd136c43492aa061b81d3
kotaro chiba <chiba-kotaro@dmm.com>
2020-01-06 12:05:52 +0900

    add: comment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,7 +24,11 @@ class CustomApp extends App<CustomProps> {
             pageProps = await Component.getInitialProps(ctx);
         }
 
-        // @see https://github.com/uruha/next-with-ts/blob/af76ea71b8aeb5d3d970834e57f065a382cf71d6/src/store/index.ts#L39
+        /**
+         * @see https://github.com/uruha/next-with-ts/blob/af76ea71b8aeb5d3d970834e57f065a382cf71d6/src/store/index.ts#L39
+         * Can't extended AppContext to include CustomProps Type,
+         * tentatively ignore ctx.store type lint.
+         */
         // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
         // @ts-ignore
         await ctx.store.execSagaTask(ctx.isServer, (dispatch: Dispatch) => {
